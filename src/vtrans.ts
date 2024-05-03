d3b66b7cbd89b70a53157a173cd642423e93d9fd
mikhail <mikhail@synzr.space>
2024-05-03 22:23:54 +0500

    fix(errors): exporting the SubtitlesError/URLError in main script

--- src/vtrans.ts
+++ src/vtrans.ts
@@ -1,4 +1,4 @@
 import { Client } from './client'
-import { TranslationError } from './errors'
+import { TranslationError, SubtitlesError, URLError } from './errors'
 
-export { Client, TranslationError }
+export { Client, TranslationError, SubtitlesError, URLError }
